1d8998317bb67d968734ba7170bd2b52d8512729
Abhishek11611 <abhisheksureshgupta11611@gmail.com>
2025-05-14 12:27:43 +0530

    Added Jwt Token Generate

---
+++ src/main/java/com/example/demo/jwt/AuthController.java
@@ -0,0 +1,39 @@
+package com.example.demo.jwt;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.requestdto.AuthenticationRequest;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@RequestBody AuthenticationRequest request) {
+ 
+        String response = userService.register(request.getUsername(), request.getPassword());
+        return ResponseEntity.ok(response);
+    }
+    @PostMapping("/login")
+    public ResponseEntity<Map<String, String>> login(@RequestBody Map<String, String> request) {
+        String username = request.get("username");
+        String password = request.get("password");
+
+
+        String token = userService.login(username, password);
+        Map<String, String> response = new HashMap<>();
+        response.put("token", token);
+        return ResponseEntity.ok(response);
+    }
+}
